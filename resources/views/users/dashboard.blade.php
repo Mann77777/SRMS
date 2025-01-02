a1fc863d6c4f3cfa1f24da2d1b675296c5f22327
maiyv <marielle.verdaluza07@gmail.com>
2025-01-02 21:54:51 +0800

    update

--- resources/views/users/dashboard.blade.php
+++ resources/views/users/dashboard.blade.php
@@ -2,8 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="csrf-token" content="{{ csrf_token() }}">  <!-- Add this line -->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">    <meta name="csrf-token" content="{{ csrf_token() }}">  <!-- Add this line -->
     <link rel="icon" href="{{ asset('images/tuplogo.png') }}" type="image/x-icon">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
